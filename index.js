8b846ca9bc0a7a552c17e7ce93f64e4c068ae86b
larimar1212 <larimarfondeur@gmail.com>
2022-07-19 10:32:27 -0400

    solution

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) { // a
+return drivers.slice(0, 2);
+
+}; // console.log(returnFirstTwoDrivers) would = Antonia, Nuru
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2)
+}; // console.log(returnaLastTwoDrivers)= Amari, No 
+
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+ // console.log(selectingDrivers[0]) = returnFirstTwoDrivers
+
+ const createFareMultiplier = function (multiplyValue) {
+    return function (value) {
+        return multiplyValue * value;
+    
+
+    };
+ };
+
+ 
